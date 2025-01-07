47f484fad0c5b6142646bf0ad9775ac7f07266bc
AhmadTawil1 <ahmadtawil.se@outlook.com>
2025-01-07 20:10:53 +0200

    Create columns, image, and description

---
+++ main.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo100.png", width=300)
+
+with col2:
+    st.title("Ahmad Tawil")
+    content = """
+        Hi! I’m a passionate software engineering student specializing in Python programming.
+        I love crafting efficient, user-friendly solutions and am dedicated to continuous learning and innovation.
+        With a strong drive to excel,
+        I’m focused on building impactful projects and advancing my career in software development.
+    """
+
+    st.info(content)
