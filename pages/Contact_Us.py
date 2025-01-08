6b544ab300f25c944def612e833bc75f4f919242
AhmadTawil1 <ahmadtawil.se@outlook.com>
2025-01-08 20:25:13 +0200

    Send email using the form

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,35 +1,19 @@
 import streamlit as st
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
+from send_email import send_email
 
 
 st.header("Contact Us")
 
 with st.form(key="email_form"):
-    User_email = st.text_input("Your email address")
-    message = st.text_area("your message")
+    user_email = st.text_input("Your email address")
+    raw_message = st.text_area("your message")
+    message = f"""\
+Subject: New email from {user_email}
+    
+From: {user_email}
+{raw_message}
+"""
     button = st.form_submit_button("Submit")
     if button:
-        # Email details
-        your_email = "ahmadtawil.se@outlook.com"
-        your_password = "AhmadTawilSe?!123"
-        subject = "New Message from Contact Form"
-
-        # Create email content
-        email_message = MIMEMultipart()
-        email_message["From"] = User_email
-        email_message["To"] = your_email  # Send to yourself
-        email_message["Subject"] = subject
-        email_body = f"Message from: {User_email}\n\n{message}"
-        email_message.attach(MIMEText(email_body, "plain"))
-
-        try:
-            # Connect to Outlook SMTP server and send the email
-            with smtplib.SMTP("smtp.office365.com", 587) as server:
-                server.starttls()  # Secure the connection
-                server.login(your_email, your_password)
-                server.send_message(email_message)
-                st.success("Your message has been sent successfully!")
-        except Exception as e:
-            st.error(f"Failed to send email: {e}")
+        send_email(message)
+        st.info("Your email was sent successfully")
