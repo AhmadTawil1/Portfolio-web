1f305dd83cbc62a2b0d80075cf785cd9757ee697
AhmadTawil1 <ahmadtawil.se@outlook.com>
2025-01-08 11:51:55 +0200

    Working on Contact_Us

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
+
 st.header("Contact Us")
+
+with st.form(key="email_form"):
+    User_email = st.text_input("Your email address")
+    message = st.text_area("your message")
+    button = st.form_submit_button("Submit")
+    if button:
+        message = message + User_email
